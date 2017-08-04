1afe0219aa12196a0b724e187f38c35607957289
kode with klossy <kodewithklossy@kodes-MacBook-Pro.local>
2017-08-04 09:51:04 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+function changeToBeach() {
+    var beachChange = document.getElementById("background");
+    beachChange.setAttribute("src", "#Hanola");
+    var byeSphere1 = document.getElementById("sphere1");
+    byeSphere1.setAttribute("visible", "false");
+    var byeSphere2 = document.getElementById("sphere2");
+    byeSphere2.setAttribute("visible", "false");
+    var byeSphere3 = document.getElementById("sphere3");
+    byeSphere3.setAttribute("visible", "false")
+}
+
+function changeToMonuments(){
+    var monumentChange = document.getElementById("background");
+    monumentChange.setAttribute("src", "#Petra");
+    var byeSphere1 = document.getElementById("sphere1");
+    byeSphere1.setAttribute("visible", "false");
+    var byeSphere2 = document.getElementById("sphere2");
+    byeSphere2.setAttribute("visible", "false");
+    var byeSphere3 = document.getElementById("sphere3");
+    byeSphere3.setAttribute("visible", "false")
+}
+
+function changeToParks(){
+    var parkChange = document.getElementById("background");
+    parkChange.setAttribute("src", "#Yellowstone");
+    var byeSphere1 = document.getElementById("sphere1");
+    byeSphere1.setAttribute("visible", "false");
+    var byeSphere2 = document.getElementById("sphere2");
+    byeSphere2.setAttribute("visible", "false");
+    var byeSphere3 = document.getElementById("sphere3");
+    byeSphere3.setAttribute("visible", "false")
+}
